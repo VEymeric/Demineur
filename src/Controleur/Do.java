a1078d3aa2efd687ad39804942b208e2a54a3ea0
Chao <Chao@172.18.4.44>
2017-03-10 11:46:19 +0100

    Superbe fonction init

--- src/Controleur/Do.java
+++ src/Controleur/Do.java
@@ -13,38 +13,23 @@ import Modele.Case;
  */
 public class Do {
     Modele.Matrice m;
-    
-    public Do(int i, int j){
+    private int nbMine = 0;
+    public Do(int i, int j, int percentMine){
         m = new Modele.Matrice(i,j);
-        m.
+        nbMine = (i * j) * percentMine/ 100;
     }
     
-    
-    private void majNumbers(int x, int y){
-        if (x>0 && y>0) { // voisin haut gauche
-            if(m.getGridInitCase(y-1,x-1) == Case.NUMBER ) gridInit[y-1][x-1].setValueI(gridInit[y-1][x-1].getValueI()+1);
-        }
-        if (y>0) { // voisin haut
-            if(gridInit[y-1][x] == Case.NUMBER ) gridInit[y-1][x].setValueI(gridInit[y-1][x].getValueI()+1);
-        }
-        if (x<getI() && y>0) { // voisin haut droit
-            if(gridInit[y-1][x+1] == Case.NUMBER ) gridInit[y-1][x+1].setValueI(gridInit[y-1][x+1].getValueI()+1);
-        }
-        if(x>0){//voisin gauche
-            if(gridInit[y][x-1] == Case.NUMBER ) gridInit[y][x-1].setValueI(gridInit[y][x-1].getValueI()+1);
-        }
-        if(x<getI()){//voisin droit
-            if(gridInit[y][x+1] == Case.NUMBER ) gridInit[y][x+1].setValueI(gridInit[y][x+1].getValueI()+1);
-        }
-        if (x>0 && y<getJ()) { // voisin bas gauche
-            if(gridInit[y+1][x-1] == Case.NUMBER ) gridInit[y+1][x-1].setValueI(gridInit[y+1][x-1].getValueI()+1);
-        }
-        if (y<getJ()) { // voisin bas
-            if(gridInit[y+1][x] == Case.NUMBER ) gridInit[y+1][x].setValueI(gridInit[y+1][x].getValueI()+1);
-        }
-        if (x<getI() && y<getJ()) { // voisin haut droit
-            if(gridInit[y-1][x+1] == Case.NUMBER ) gridInit[y-1][x+1].setValueI(gridInit[y-1][x+1].getValueI()+1);
-        }
+    private void generateMatrice(int x, int y){//Genère une matrice de jeu à partir du premier clic en x;y
+       int mineUnused = nbMine;
+       while(mineUnused>0){
+            int randI = (int) (Math.random() * m.getI());
+            int randJ = (int) (Math.random() * m.getJ());
+            if (Case.NUMBER.equals(m.getGridInitCase(randJ, randI)) && (randI != x && randJ != y)) {
+                m.setGridInitCase(Case.MINE, randJ, randI);
+                //majNumbers(randI, randJ);
+                mineUnused--;
+            }
+       }
     }
     
     
