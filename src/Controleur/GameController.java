0a91362350144ddb72d88dd80b809c05524246ee
Chao <Chao@172.18.4.44>
2017-03-11 21:33:28 +0100

    visibility

---
+++ src/Controleur/GameController.java
@@ -0,0 +1,49 @@
+package Controleur;
+
+import Modele.Matrice;
+import View.Print;
+
+public class GameController{
+    Matrice m;
+    private Print gameView;
+            
+    public GameController(int x, int y, int percentMine){
+        m = new Modele.Matrice(x,y,(x * y) * percentMine/ 100);
+    }
+    
+    //Recupère les ordre donnés par la console et les traites, si inconnu affiche l'help;
+    public void order(String go,boolean ok) {
+        String[] cr = go.split(" ");
+        switch (cr[0]) {
+            case "d":
+                if (cr.length == 3) {
+                    //m.print(Integer.parseInt(cr[1]), Integer.parseInt(cr[2]));
+                    break;
+                } else {
+                    help();
+                    break;
+                }
+            case "q":
+                //stop(" give up ");
+                break;
+            case "m":
+                if (cr.length == 4) {
+                    //mark(Integer.parseInt(cr[1]), Integer.parseInt(cr[2]), cr[3]);
+                    break;
+                } else {
+                    help();
+                    break;
+                }
+            default:
+                help();
+        }
+    }
+    
+    //affiche les diffèrentes commandes possibles
+    private void help() {
+        System.out.println(" Not complete order ");
+        System.out.println(" d x y -> show this case ");
+        System.out.println(" m x y ?/! -> mark case with ?/!");
+        System.out.println(" q -> quit ");
+    }
+}
