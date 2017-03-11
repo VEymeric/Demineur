beeabb39862eb7917bf82e78ee777010b9aa3783
Chao <Chao@172.18.4.44>
2017-03-11 22:04:36 +0100

    rename "i" and "j" for width and height

--- src/Modele/Matrice.java
+++ src/Modele/Matrice.java
@@ -12,14 +12,14 @@ import java.util.Observable;
  * @author gaetane
  */
 public class Matrice extends Observable {
-    int i=0,j=0,mine=0;
+    int width=0,height=0,mine=0;
     Case[][] gridHide;
     Case[][] gridInit;
 
-    public Matrice( int i, int j, int nbMine){
-        this.i = i;
-        this.j = j;
-        generateMatrice(i,j,nbMine);
+    public Matrice(int width, int height, int nbMine){
+        this.width = width;
+        this.height = height;
+        generateMatrice(width,height,nbMine);
     }    
   
     public Case getGridInitCase(int y, int x) {
@@ -29,51 +29,52 @@ public class Matrice extends Observable {
     public void setGridInitCase(Case c, int i,int j) {
         this.gridInit[j][i] =  c ;
     }
-    
-    public int getI() {
-        return i;
+
+        public int getWidth() {
+        return width;
     }
 
-    public void setI(int i) {
-        this.i = i;
+    public void setWidth(int width) {
+        this.width = width;
     }
 
-    public int getJ() {
-        return j;
+    public int getHeight() {
+        return height;
     }
 
-    public void setJ(int j) {
-        this.j = j;
+    public void setHeight(int height) {
+        this.height = height;
     }
         
     private void generateInit(){ // tableau de valeur initialisé
-        for(int x = 0; x < getI(); x++){
-            for(int y = 0 ; y < getJ() ; y++){
+        for(int x = 0; x < getWidth(); x++){
+            for(int y = 0 ; y < getHeight() ; y++){
                 gridInit[y][x] = Case.NUMBER;
             }
         }
     }    
     private void generatHide(){ // tableau caché initialisé
-        for(int x = 0; x < getI(); x++){
-            for(int y = 0 ; y < getJ() ; y++){
+        for(int x = 0; x < getWidth(); x++){
+            for(int y = 0 ; y < getHeight() ; y++){
                 gridHide[y][x] = Case.UNKNOW;
             }
         }        
     }
     
-    private void generateMatrice(int i, int j, int nbMine){//Genère une matrice de jeu à partir du premier clic en x;y
+    //Genère une matrice de jeu à partir du premier clic en x;y
+    private void generateMatrice(int xClic, int yClic, int nbMine){
        int mineUnused = nbMine;
        while(mineUnused>0){
-            int randI = (int) (Math.random() * getI());
-            int randJ = (int) (Math.random() * getJ());
-            if (Case.NUMBER.equals(getGridInitCase(randJ, randI)) && (randI != i && randJ != j)) {
+            int randI = (int) (Math.random() * getWidth());
+            int randJ = (int) (Math.random() * getHeight());
+            if (Case.NUMBER.equals(getGridInitCase(randJ, randI)) && (randI != xClic && randJ != yClic)) {
                 setGridInitCase(Case.MINE, randJ, randI);
                 majNumbers(randI, randJ);
                 mineUnused--;
             }
        }
-       for(int x=0; x<getI(); x++){
-           for(int y=0; y<getJ(); y++){
+       for(int x=0; x<getWidth(); x++){
+           for(int y=0; y<getHeight(); y++){
                if(getGridInitCase(y, x).getValueI() == 0){
                    setGridInitCase(Case.ALONE, y,x);
                }
@@ -81,6 +82,7 @@ public class Matrice extends Observable {
        }
     }
     
+    //fonction qui ajoute 1 à toutes les cases autour d'une bombe : efficace pour initiliser
     private void majNumbers(int x, int y){
         if (x>0 && y>0) { // voisin haut gauche
             if(getGridInitCase(y-1,x-1) == Case.NUMBER ) getGridInitCase(y-1, x-1).setValueI(getGridInitCase(y-1, x-1).getValueI()+1);
@@ -88,22 +90,22 @@ public class Matrice extends Observable {
         if (y>0) { // voisin haut
             if(getGridInitCase(y-1,x) == Case.NUMBER ) getGridInitCase(y-1, x).setValueI(getGridInitCase(y-1, x).getValueI()+1);
         }
-        if (x<getI() && y>0) { // voisin haut droit
+        if (x<getWidth() && y>0) { // voisin haut droit
             if(getGridInitCase(y-1,x+1) == Case.NUMBER ) getGridInitCase(y-1, x+1).setValueI(getGridInitCase(y-1, x+1).getValueI()+1);
         }
         if(x>0){//voisin gauche
             if(getGridInitCase(y,x-1) == Case.NUMBER ) getGridInitCase(y, x-1).setValueI(getGridInitCase(y, x-1).getValueI()+1);
         }
-        if(x<getI()){//voisin droit
+        if(x<getWidth()){//voisin droit
             if(getGridInitCase(y,x+1) == Case.NUMBER ) getGridInitCase(y, x+1).setValueI(getGridInitCase(y, x+1).getValueI()+1);
         }
-        if (x>0 && y<getJ()) { // voisin bas gauche
+        if (x>0 && y<getHeight()) { // voisin bas gauche
             if(getGridInitCase(y+1,x-1) == Case.NUMBER ) getGridInitCase(y+1, x-1).setValueI(getGridInitCase(y+1, x-1).getValueI()+1);
         }
-        if (y<getJ()) { // voisin bas
+        if (y<getHeight()) { // voisin bas
             if(getGridInitCase(y+1,x) == Case.NUMBER ) getGridInitCase(y+1, x).setValueI(getGridInitCase(y+1, x).getValueI()+1);
         }
-        if (x<getI() && y<getJ()) { // voisin haut droit
+        if (x<getWidth() && y<getHeight()) { // voisin haut droit
             if(getGridInitCase(y-1,x+1) == Case.NUMBER ) getGridInitCase(y-1, x+1).setValueI(getGridInitCase(y-1, x+1).getValueI()+1);
         }
     }
