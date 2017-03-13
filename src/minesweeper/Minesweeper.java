a7793ce01af08d661a538a74f56386ff3c5211ee
Chao <Chao@172.18.4.44>
2017-03-13 12:30:21 +0100

    bonne initialisation de la matrice init

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package minesweeper;
 
 import Controleur.GameController;
 import java.util.Scanner;
 
-/**
- *
- * @author gaetane
- */
 public class Minesweeper {
 
     /**
