32c87390f006407390e7f1c4e7edd1491b25605f
Chao <Chao@172.18.4.44>
2017-03-13 14:43:19 +0100

    utilisation des isBidule retournant un bool

--- src/Modele/Case.java
+++ src/Modele/Case.java
@@ -4,13 +4,14 @@ package Modele;
 public class Case {
     private int x,y;
     private int bombes;
-    private CaseInit etat;
-    private CaseHide cache;
+    private CaseInit etat; //mine ; alone ; number
+    private CaseHide cache;// unknow , flag , hide, show
     
     public Case(int x,int y){
         this.x = x;
         this.y = y;
         this.etat = CaseInit.NUMBER ;
+        this.cache = CaseHide.HIDE;
     }
 
     public Case(int x,int y, CaseHide cache){
@@ -43,6 +44,27 @@ public class Case {
         this.etat = etat;
     }
     
+    public boolean isMine(){
+        return this.etat==CaseInit.MINE;
+    }
+    public boolean isAlone(){
+        return this.etat==CaseInit.ALONE;
+    }
+    public boolean isNumber(){
+        return this.etat==CaseInit.NUMBER;
+    }
+    public boolean isHide(){
+        return this.cache==CaseHide.HIDE;
+    }
+    public boolean isUnknow(){
+        return this.cache==CaseHide.UNKNOW;
+    }
+    public boolean isFlag(){
+        return this.cache==CaseHide.FLAG;
+    }
+    public boolean isShow(){
+        return this.cache==CaseHide.SHOW;
+    }
     
     
 }
